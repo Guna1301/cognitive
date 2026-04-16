875377a920bb217f398f22a2bec94cf6d426e0af
Guna1301 <gskotipalli2005@gmail.com>
2026-04-16 18:48:44 +0530

    lightweight embeddings

--- ML/app/services/rag_service.py
+++ ML/app/services/rag_service.py
@@ -4,9 +4,7 @@ from chromadb.utils import embedding_functions
 
 client = chromadb.Client()
 
-embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2"
-)
+embedding_function = embedding_functions.DefaultEmbeddingFunction()
 
 collection = client.get_or_create_collection(
     name="cognitive_knowledge",
